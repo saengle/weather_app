c85c5f5b5eacd6a962f582288124055cd1d5cef7
saengle <zohv817@gmail.com>
2022-08-14 13:58:34 +0900

    ADD: weather_app
    Start weather_app project

---
+++ lib/weather_app/weather_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class WeatherScreen extends StatefulWidget {
+  const WeatherScreen({Key? key}) : super(key: key);
+
+  @override
+  State<WeatherScreen> createState() => _WeatherScreenState();
+}
+
+class _WeatherScreenState extends State<WeatherScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+          appBar: AppBar(
+            title: Text('Weather App'),
+          ),
+          body: Container(
+
+          ),
+        );
+  }
+}
